7272024601e7e4f73ab2cfd359d273d4437e08a1
DavidGonzalezJ <davidgonzalezj97@gmail.com>
2019-05-25 15:16:22 +0200

    Here we go again

---
+++ app/src/main/java/es/ucm/gdv/bombardero20/BombAndroid.java
@@ -0,0 +1,13 @@
+package es.ucm.gdv.bombardero20;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BombAndroid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bomb_android);
+    }
+}
